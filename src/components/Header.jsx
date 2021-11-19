fd9c6fdb22362888375b884b99b15d9aaf7fbc3b
tomi <dev.dreissiger@gmail.com>
2021-11-19 20:50:29 +0100

    header work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,121 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+
 export default function Header() {
+    const mediaQuery = window.matchMedia('(max-width: 1023px)');
+
+    const [isSmallScreen, setIsSmallScreen] = useState(mediaQuery.matches);
+    const [isVerticalMenuVisible, setIsVerticalMenuVisible] = useState(false);
+
+    useEffect(() => {
+        const onMediaChange = (e) => {
+            setIsSmallScreen(e.matches);
+        };
+
+        mediaQuery.addEventListener('change', onMediaChange);
+
+        return () => {
+            mediaQuery.removeEventListener('change', onMediaChange);
+        };
+    }, []);
+
+    const links = (
+        <>
+            <Link href="#home">Home</Link>
+            <Link href="#about">About</Link>
+            <Link href="#features">Features</Link>
+            <Link href="#pricing">Pricing</Link>
+            <Link href="#trainers">Trainers</Link>
+            <Link href="#blogs">Blogs</Link>
+        </>
+    );
+
     return (
-        <div>feri</div>
+        <Container data-component="header">
+            <Logo href="#">
+                <span>Be</span>Fit
+            </Logo>
+
+            {isSmallScreen && <VerticalMenu isVerticalMenuVisible={isVerticalMenuVisible}>
+                {links}
+            </VerticalMenu>}
+
+            {isSmallScreen && <MenuIcon className="bx bx-menu" onClick={() => setIsVerticalMenuVisible(!isVerticalMenuVisible)}></MenuIcon>}
+
+            {!isSmallScreen && <HorizontalMenu>
+                {links}
+            </HorizontalMenu>}
+        </Container>
     );
 }
+
+const Container = styled.header`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: var(--black);
+    padding: 1rem 2rem;
+
+    @media(min-width: 1024px) {
+        padding: 1rem 4rem;
+    }
+`;
+
+const Logo = styled.a`
+    font-weight: 600;
+    font-size: 1.5rem;
+    color: var(--white);
+    text-decoration: none;
+
+    span {
+        color: var(--red);
+    }
+
+    @media(min-width: 1536px) {
+        font-size: 2rem;
+    }
+`;
+
+const VerticalMenu = styled.nav`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 70%;
+    height: 100vh;
+    background-color: var(--black);
+    padding: 2rem 2rem;
+    display: flex;
+    flex-direction: column;
+    transition: 200ms;
+    transform: translate3d(${({ isVerticalMenuVisible }) => isVerticalMenuVisible ? '0' : '-100%'}, 0, 0);
+`;
+
+const HorizontalMenu = styled.nav`
+
+`;
+
+const Link = styled.a`
+    color: var(--white);
+    margin-bottom: 1rem;
+    margin-right: 1rem;
+    font-size: 1rem;
+    text-decoration: none;
+
+    :hover {
+        color: var(--red);
+    }
+
+    @media(min-width: 1536px) {
+        font-size: 1.5rem;
+    }
+`;
+
+const MenuIcon = styled.i`
+    color: var(--white);
+    font-size: 2rem;
+    cursor: pointer;
+`;
+
